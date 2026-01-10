83deb29fbb6aa8a3ca5cdb0bc7a1af83b07cae69
Valentin Monod <vmonod5@gmail.com>
2026-01-10 14:21:27 +0100

    video analysis v1

--- src/Pendulum.jl
+++ src/Pendulum.jl
@@ -1,15 +1,34 @@
 module Pendulum
 
+# simulation libs
 using Plots
 using DifferentialEquations
+
+# video analysis libs
+using OpenCV
+using Optim
+using Statistics
 
 include("types.jl")
 include("physics.jl")
 include("plot_manager.jl")
 include("double_pendulum.jl")
+include("video_analysis.jl")
 
 function main()
-    pendulum = launch_simulation()
+    θ1_0, θ2_0, ω1_0, ω2_0, m1, m2, L1, L2 = analyze_video("res/video.mp4")
+
+    pendulum = launch_simulation(
+        θ1_0=θ1_0,
+        θ2_0=θ2_0,
+        ω1_0=ω1_0,
+        ω2_0=ω2_0,
+        m1=m1,
+        m2=m2,
+        L1=L1,
+        L2=L2
+    )
+
     plot_stats(pendulum)
     plot_pendulum(pendulum)
 end
